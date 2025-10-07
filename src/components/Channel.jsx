f62eedf1539f7688ccedccbe8eb4bd3b484bae2b
Niraj1903 <niraj.singh1903@outlook.com>
2025-10-07 18:11:37 +0530

    form styled using tailwindcss

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -1,44 +1,58 @@
 const Channel = () => {
   return (
     <>
-      <div>
+      <div className="max-w-xl mx-auto p-4 bg-white shadow-lg rounded-lg">
         <form>
-          <div className="mt-3 flex justify-center ">
-            <label className="mr-5 p-2 " htmlFor="name  w-1/6">
-              Name :
+          <div className="mt-5 flex justify-between items-center">
+            <label
+              className="mr-5 text-lg font-medium text-gray-700"
+              htmlFor="name"
+            >
+              Name:
             </label>
             <input
-              className="border border-black p-1 ml-5 w-[200px]"
+              className="border border-gray-300 p-2 w-[200px] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="text"
               placeholder="Name"
               id="name"
               name="name"
             />
           </div>
-          <div className="mt-3 flex justify-center">
-            <label className="mr-5 p-2 " htmlFor="email  w-1/6">
-              Email :
+
+          <div className="mt-5 flex justify-between items-center">
+            <label
+              className="mr-5 text-lg font-medium text-gray-700"
+              htmlFor="email"
+            >
+              Email:
             </label>
             <input
-              className="border border-black p-1 ml-5 w-[200px]"
+              className="border border-gray-300 p-2 w-[200px] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="email"
-              placeholder="email"
+              placeholder="Email"
               name="email"
             />
           </div>
-          <div className="mt-3 flex justify-center">
-            <label className="mr-5 p-2 " htmlFor="channel w-1/6">
-              Channel
+
+          <div className="mt-5 flex justify-between items-center">
+            <label
+              className="mr-5 text-lg font-medium text-gray-700"
+              htmlFor="channel"
+            >
+              Channel:
             </label>
             <input
-              className="border border-black p-1 ml-5 w-[200px]"
+              className="border border-gray-300 p-2 w-[200px] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="text"
               name="channel"
               id="channel"
             />
           </div>
-          <div className="mt-3 flex justify-center">
-            <button className="ml-5 p-2">Submit</button>
+
+          <div className="mt-6 flex justify-center">
+            <button className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition-colors">
+              Submit
+            </button>
           </div>
         </form>
       </div>
